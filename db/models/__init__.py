ea233863bcd655d12c953c2ac1a2e6c2a8373c15
Jacob Gardner <juhucub@gmail.com>
2026-03-25 10:23:27 -0700

    _init_ new tables

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -1,7 +1,15 @@
 #db model exports
 
 from db.models.orders import Order
+from db.models.portfolio import PortfolioAccountState, TradeHistory
 from db.models.positions import Position
 from db.models.snapshots import MarketDataSnapshot, PortfolioSnapshot
 
-__all__ = ["Order", "Position", "PortfolioSnapshot", "MarketDataSnapshot"]
+__all__ = [
+    "Order", 
+    "Position", 
+    "PortfolioSnapshot", 
+    "MarketDataSnapshot",
+    "PortfolioAccountState",
+    "TradeHistory",
+]
